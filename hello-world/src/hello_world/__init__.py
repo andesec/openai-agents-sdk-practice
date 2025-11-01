a84ca0652a5ca5ee2a86d4d5cecea054ef29acfd
Ande <andesec1@gmail.com>
2025-11-01 16:43:38 +0500

    Removed entries for printing the keys

--- hello-world/src/hello_world/__init__.py
+++ hello-world/src/hello_world/__init__.py
@@ -10,5 +10,3 @@ __all__ = ["main"]
 def main() -> None:
     load_dotenv()
     print("Hello from hello-world!")
-    print("OPENAI_API_KEY:", os.environ.get("OPENAI_API_KEY"))
-    print("GEMINI_API_KEY:", os.environ.get("GEMINI_API_KEY"))
